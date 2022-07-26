d7e120777b97bb67bf163017b33750bf237ae75a
nknrw <nknrw@yandex.ru>
2022-07-26 12:42:04 +0300

    feat: add twopart jokes

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const jokeContainer = document.querySelector('.joke-container');
-const jokeElement = document.querySelector('.joke');
+const jokeSetup = document.querySelector('.setup');
+const jokePunchline = document.querySelector('.punchline');
 const getJokeBtn = document.querySelector('.get-joke-btn');
 
+
+
 function returnJoke() {
-    jokeElement.textContent = ''
-    fetch(`https://v2.jokeapi.dev/joke/Any?type=single`)
+    jokeSetup.textContent = '';
+    jokePunchline.textContent = '';
+    fetch(`https://v2.jokeapi.dev/joke/Any`)
     .then((res) => {
         return res.json();
     })
     .then((data) => {
-        jokeElement.textContent = data.joke;
+        if (data.type == "twopart") {
+            jokeSetup.textContent = data.setup;
+            jokePunchline.textContent = data.delivery;
+        } else {
+            jokeSetup.textContent = data.joke;
+        }
+        console.log(data)    
     });
     window.scroll(0, 0)
 };
